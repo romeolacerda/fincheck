c136c8940712244dad4eebcde5070b65b65533a8
Romeo Ebrahim <romeolacerdafarias@gmail.com>
2025-04-05 09:36:06 -0300

    dockerize api

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -6,6 +6,12 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get('hello')
+  getHello(): string {
+    return 'Hello World';
+  }
+  
+  
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
